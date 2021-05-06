522887a489e1098f4de5cdda89d5b1b1dce5ab0f
Fitsum A <fitsumcs@gmail.com>
2021-05-06 17:31:45 +0400

    Starting a simple contact form plugin

---
+++ mycontact.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Simple Contact Form
+ * Plugin URI:        https://contactform.com
+ * Description:       A simple contact form plugin that is used for displaying a contact form 
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Fitsum A. 
+ * Author URI:        https://fa.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ */
